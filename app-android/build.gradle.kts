c7444710773858810f2e74dac48cd44d253d2a24
admin <admin@inqueuet.com>
2025-11-20 11:37:06 +0900

    内部バージョン変更

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "0.4"
+        versionName = "0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
