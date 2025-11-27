326677417bef2d0e856df99c85113a5f4b255b42
admin <admin@inqueuet.com>
2025-11-27 17:33:53 +0900

    内部バージョン変更

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "com.valoser.futacha"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
