3bc9a7b54094cb25ee4475634095151722f189b2
admin@inqueuet.com <admin@inqueuet.com>
2025-12-28 00:43:54 +0900

    バグ修正

--- shared/src/androidMain/kotlin/ui/board/PlatformVideoPlayer.android.kt
+++ shared/src/androidMain/kotlin/ui/board/PlatformVideoPlayer.android.kt
@@ -14,6 +14,7 @@ import androidx.media3.common.PlaybackException
 import androidx.media3.common.Player
 import androidx.media3.common.VideoSize
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.ui.AspectRatioFrameLayout
 import androidx.media3.ui.PlayerView
 
 @Composable
@@ -52,6 +53,7 @@ actual fun PlatformVideoPlayer(
         factory = {
             PlayerView(context).apply {
                 useController = true
+                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_FIT
                 this.player = player
             }
         },
