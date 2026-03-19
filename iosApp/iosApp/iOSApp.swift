4ed0c27b077fef41f1315cf77af2ccc26711d5cd
root <you@example.com>
2026-03-19 21:39:34 +0900

    保守性向上

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -2,12 +2,16 @@ import SwiftUI
 import UIKit
 import shared
 import GoogleMobileAds
+import FirebaseCore
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
+        if let _ = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") {
+            FirebaseApp.configure()
+        }
         MobileAds.shared.start(completionHandler: nil)
         return true
     }
